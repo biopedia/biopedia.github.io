f973282c40b34d6e309d88c0e5fbde058fce33ab
piyurtw1-alt <piyurtw1@gmail.com>
2026-07-10 22:52:28 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "biopedia-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/privacy.html",
+  "/subject.html",
+  "/css/style.css",
+  "/js/data.js",
+  "/js/main.js",
+  "/js/subject.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
